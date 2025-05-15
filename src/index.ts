a169fc80cbb579bd7fce449beb0a49ef3fc9eff3
Violet Caulfield <violet@cosmonautical.cloud>
2025-05-14 22:14:24 -0500

    update index

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ var env = process.env;
 // @ts-ignore :(
 import { patreon } from 'patreon'
 
-var patreonApi = patreon(env.PATREON_ACCESS_TOKEN)
-
-var redirectUrl = "https://localhost:8080/callback"
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+
+		// @ts-ignore :(
+		var patreonApi = patreon(env.PATREON_ACCESS_TOKEN)
 
 		return patreonApi('/api/oauth2/v2/campaigns/anultravioletaurora/members')
 			.then((response: any) => {
